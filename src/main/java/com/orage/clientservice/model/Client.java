db70ca532646738575d7d29560b82246763b0902
itawareness <ashu.ram@oragetechnologies.com>
2024-11-08 15:22:01 +0530

    client test by ashu

--- src/main/java/com/orage/clientservice/model/Client.java
+++ src/main/java/com/orage/clientservice/model/Client.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-//TEst comment     
+//hello tst by ashu
 
 @Entity
 @Table(name = "clients")
