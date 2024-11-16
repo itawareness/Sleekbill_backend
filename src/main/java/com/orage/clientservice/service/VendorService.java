dd80e502931b9b0801941faf8d00f44d9ba26898
itawareness <83878938+itawareness@users.noreply.github.com>
2024-11-16 18:39:08 +0530

    Update VendorService.java

--- src/main/java/com/orage/clientservice/service/VendorService.java
+++ src/main/java/com/orage/clientservice/service/VendorService.java
@@ -2,12 +2,12 @@ package com.orage.clientservice.service;
 
 import java.util.List;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.orage.clientservice.model.Client;
 import com.orage.clientservice.model.Vendor;
@@ -16,7 +16,16 @@ import com.orage.clientservice.repository.VendorRepository;
 
 @Service
 public class VendorService {
+	
+    @Autowired
+    private ClientRepository clientRepository;
 
+    @Autowired
+    private  VendorRepository vendorRepository;
+
+
+
+    // Add new vendor
     public Vendor saveVendor(Vendor vendor) {
         // Save the vendor to the Vendor table
         Vendor savedVendor = vendorRepository.save(vendor);
@@ -24,12 +33,42 @@ public class VendorService {
         // If the vendor should also be treated as a client, save to the Client table
         if (vendor.isUseAsClient()) {
             Client client = new Client();
-            mapVendorToClient(vendor, client);
+            client.setCompanyName(vendor.getCompanyName());
+            client.setPhone(vendor.getPhone());
+            client.setEmail(vendor.getEmail());
+            client.setGstTreatment(vendor.getGstTreatment());
+            client.setGstin(vendor.getGstin());
+            client.setPan(vendor.getPan());
+            client.setVat(vendor.getVat());
+            client.setWebsite(vendor.getWebsite());
+            client.setUseAsVendor(true); // Indicating this client can also act as a vendor
+
+            // Save the client to the Client table
             clientRepository.save(client);
         }
 
         return savedVendor;
     }
+    
+    
 
+    public Page<Vendor> getVendors(int page, int size, String search) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        if (search != null || !search.isEmpty()) {
+            return vendorRepository.findByCompanyNameContainingIgnoreCase(search, pageable);
+
+        }
+            return vendorRepository.findAll(pageable); // If no search, return all vendors with pagination
+
+    }
+
+    // Method to delete all clients by their IDs
+    @Transactional
+    public void deleteAllById(List<Long> vendorIds) {
+        // If vendorIds is empty, do nothing
+        if (vendorIds != null && !vendorIds.isEmpty()) {
+            vendorRepository.deleteAllByIdIn(vendorIds);
+        }
     }
 }
